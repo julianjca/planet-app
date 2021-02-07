c3dc19ff1c97189b42942c335be6c33987ba94b2
Julian Anderson <julian@jumpcut.com>
2021-02-07 15:12:22 +0700

    adding loader

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
+import ClipLoader from "react-spinners/ClipLoader";
 
 import styles from '../styles/Home.module.scss'
 
@@ -11,20 +12,27 @@ const HomePage = ({ data }) => {
   const [planets, setPlanets] = useState(data.results)
   const [next, setNext] = useState(data.next)
 
+  const [isLoading, setIsLoading] = useState(false)
+  const [isLoadingMore, setIsLoadingMore] = useState(false)
+
   const searchPlanet = async () => {
+    setIsLoading(true)
     const res = await fetch(`https://swapi.dev/api/planets/?search=${searchInput}`)
     const resData = await res.json()
 
+    setIsLoading(false)
+
     setPlanets(resData.results)
+    setNext(resData.next)
   }
 
   const loadMore = async () => {
-    console.log(next)
     if(next) {
+      setIsLoadingMore(true)
       const res = await fetch(next)
       const resData = await res.json()
 
-      console.log(resData)
+      setIsLoadingMore(false)
       setPlanets([...planets, ...resData.results])
       setNext(resData.next)
     }
@@ -62,7 +70,21 @@ const HomePage = ({ data }) => {
           handleSearchChange={handleSearchChange} 
           searchInput={searchInput}
         />
-        <Planets planets={planets} />
+        {
+          isLoading ? (
+            <ClipLoader color="#ffffff" loading={isLoading} size={50} />
+          ) : (
+            <>
+            
+              <Planets planets={planets} />
+              {
+                isLoadingMore && (
+                  <ClipLoader color="#ffffff" loading={isLoadingMore} size={50} />
+                )
+              }
+            </>
+          )
+        }
       </main>
     </div>
   )
